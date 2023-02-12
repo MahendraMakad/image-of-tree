0c7dd3be5fcef515a9b5742763c7f75ba24f2e68
MahendraMakad <87107054+MahendraMakad@users.noreply.github.com>
2023-02-12 23:56:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+var html = "";
+
+function printPattern() {
+    //removing existing content of table which is stored using html variable
+    html="";
+
+    var redbox = document.getElementById("box1").innerHTML;
+    var whitebox = document.getElementById("box2").innerHTML;
+    let n = document.getElementById("size").value;
+    n = parseInt(n);
+    let k = n;
+    for (let i = 1; i <= n; i++) {
+        html += "<tr>";
+        for (let j = 1; j < k; j++) {
+            html += "<td>";
+            html += whitebox;
+            html += "</td>";
+        }
+        for (let j = 1; j <= i; j++) {
+
+            html += "<td>";
+            html += redbox;
+            html += "</td>";
+            if (j == i) break;
+            html += "<td>";
+            html += whitebox;
+            html += "</td>";
+
+        }
+        if (i != n) {
+            for (let j = 1; j < k; j++) {
+                html += "<td>";
+                html += whitebox;
+                html += "</td>";
+            }
+        }
+        html += "</tr>";
+        k--;
+    }
+    
+    document.getElementById("table-1").innerHTML = html;
+}
+
+
